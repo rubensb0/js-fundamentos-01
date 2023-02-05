a891a8b7ee7a1b5e1390a13c19a081f3953b69a3
rubensb0 <rsbbuzon2@gmail.com>
2023-02-05 12:29:48 +0100

    lesson 1 variables with javaScript

---
+++ 01-variables.js
@@ -0,0 +1,24 @@
+var greetings = 'hola desde'
+var school = 'pmba'
+
+var greetingsFromSchool = greetings + ' ' + school
+
+console.log(greetingsFromSchool)
+
+// holaQueTal (camelCase)
+// FINAL_TEXT (PASCAL_CASE)
+
+// string
+// number
+// boolean 
+// array 
+// object 
+
+var ageAsString = 'age'
+var ageAsNumber = 30
+
+var arrayOfValues = [30, '30', false, true, 4]
+var objectOfValues = { 
+    age: 30,
+    ageAsNumber: ageAsNumber
+}
